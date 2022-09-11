52484084c06c334845298e02825e74b7a0821727
Romain <romainlev@gmail.com>
2022-09-11 22:11:17 +0200

    up UX

--- src/hook/SlideOnWindow.hook.ts
+++ src/hook/SlideOnWindow.hook.ts
@@ -7,19 +7,29 @@ const SlideOnWindow = (moveToPourcentage: number): useStateHamburger => {
   const [isHamburgerOpen, setIsHamburgerOpen] = useState<boolean>(false);
   const [clientXDown, setClientXDown] = useState<number>(0);
   const [clientXUp, setClientXUp] = useState<number>(0);
-  const pointerup = (e: PointerEvent) => {
+  const pointerUp = (e: PointerEvent) => {
     setClientXUp(e.clientX);
   };
-  const pointerdown = (e: PointerEvent) => {
+  const pointerDown = (e: PointerEvent) => {
     setClientXDown(e.clientX);
   };
-  useEffect(() => {
-    window.addEventListener("pointerup", pointerup);
-    window.addEventListener("pointerdown", pointerdown);
+  const touchEnd = (e: TouchEvent) => {
+    setClientXUp(e.changedTouches[0].clientX);
+  };
+  const touchStart = (e: TouchEvent) => {
+    setClientXDown(e.changedTouches[0].clientX);
+  };
 
+  useEffect(() => {
+    window.addEventListener("touchstart", touchStart);
+    window.addEventListener("touchend", touchEnd);
+    window.addEventListener("pointerup", pointerUp);
+    window.addEventListener("pointerdown", pointerDown);
     return () => {
-      window.removeEventListener("pointerdown", pointerdown);
-      window.addEventListener("pointerup", pointerup);
+      window.removeEventListener("touchstart", touchStart);
+      window.removeEventListener("touchend", touchEnd);
+      window.removeEventListener("pointerup", pointerUp);
+      window.removeEventListener("pointerdown", pointerDown);
     };
   }, []);
 
