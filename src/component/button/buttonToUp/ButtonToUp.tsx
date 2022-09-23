628c878afc391b358a1cd6afa0223617cb6b013a
Romain <romainlev@gmail.com>
2022-09-23 15:07:24 +0200

    up

--- src/component/button/buttonToUp/ButtonToUp.tsx
+++ src/component/button/buttonToUp/ButtonToUp.tsx
@@ -3,7 +3,8 @@ import "./buttonToUp.scss";
 import upPage from "../../../asset/svg/upPage.svg";
 export default function ButtonToUp() {
   return (
-    <a href="?action=#" className="buttonToUp">
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    <a href="#" className="buttonToUp">
       <img src={upPage} alt="revenir en haut de la page" />
     </a>
   );
