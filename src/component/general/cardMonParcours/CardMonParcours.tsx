add7ea3ae9e616e62691eac4ca2f8b6d08d33569
Romain <romainlev@gmail.com>
2022-09-28 15:33:47 +0200

    ok

--- src/component/general/cardMonParcours/CardMonParcours.tsx
+++ src/component/general/cardMonParcours/CardMonParcours.tsx
@@ -8,10 +8,10 @@ export default function CardsMonParcours() {
     <div className="monParcours">
       <h3>Mon Parcours</h3>
       <p>
-        Aprés une expérience de 7 ans en tant que professeur de maths sciences
+        Après une expérience de 7 ans en tant que professeur de maths sciences
         dans l'enseignement, j'ai décidé de faire une reconversion
-        professionnelle dans le developpement web. Passioné d'informatique j'ai
-        appris en autodidact la programmation pendant mon temps libre. La
+        professionnelle dans le développement web. Passionné d'informatique j'ai
+        appris en autodidacte la programmation pendant mon temps libre. La
         logique, la créativité et la conception sont des domaines dans lesquels
         je m'épanouis.
       </p>
